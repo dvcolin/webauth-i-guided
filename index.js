b69108375a4ebb684ea5910b646d9ec453779a13
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-16 10:46:07 -0700

    trying to get validateCredentials middlware to work

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send("It's alive!");
+server.get('/users', validateCredentials, (req, res) => {
+  Users.find()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(err => {
+    res.status(500).json({ error: 'Unexpected error' })
+  })
 });
 
 
@@ -45,10 +51,11 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (bcrypt.compareSync(password, hash)) {
+      // check password
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: 'You cannot pass!' });
       }
     })
     .catch(error => {
@@ -66,3 +73,25 @@ server.get('/api/users', (req, res) => {
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
+function validateCredentials(req, res, next) {
+  let { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Unexpected error' });
+    })
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+}
